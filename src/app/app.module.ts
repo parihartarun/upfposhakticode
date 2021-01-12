9e0bd7011e547f9d4b5e28a4c21dbad01d7b4666
vishal patil <vishalp755@gmail.com>
2021-01-12 15:20:16 +0530

    change for  landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +13,9 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,6 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  slides = [
+    { image: '../../../assets/newdesign/img/slider/1.jpg', text: 'Connect with buyers and exporters' },
+    { image: '../../../assets/newdesign/img/slider/3.jpg', text: 'Get information about FPO registration' },
+    { image: '../../../assets/newdesign/img/slider/4.jpg', text: 'Get information about seeds, fertilizers, agricultural implements etc' }
+  ];
+  noWrapSlides = false;
+  showIndicator = true;
 
   ngOnInit(): void {
   }
