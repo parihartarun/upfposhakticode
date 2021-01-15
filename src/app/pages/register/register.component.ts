d584c1d239c3f84fa3626a09a092fb7faa51f363
vishal patil <vishalp755@gmail.com>
2021-01-15 14:48:08 +0530

    make chnage for farmer regitration

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  register: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,) { }
 
   ngOnInit() {
+    this.register = this.fb.group({
+      accountNo: ['', Validators.required],
+      agency: ['', Validators.required],
+      bankRefId: ['', Validators.required],
+      blockRef: ['', Validators.required],
+      category: ['', Validators.required],
+      createdBy: ['', Validators.required],
+      deleted: [true],
+      enabled: [true],
+      educationId: ['', Validators.required],
+      farmerAddress: ['', Validators.required],
+      farmerAdhaar: ['', Validators.required],
+      farmerId: ['', Validators.required],     
+      farmerLotNo: ['', Validators.required],
+      farmerMob: ['', Validators.required],
+      farmerName: ['', Validators.required],
+      figRefId: ['', Validators.required],
+      fpoRefId: ['', Validators.required],
+      ifscCode: ['', Validators.required],
+      kccno: ['', Validators.required],
+      parantsName: ['', Validators.required],
+      pincode: ['', Validators.required],
+      registerDate: ['', Validators.required],
+      slaRefId: ['', Validators.required],
+      stateref: ['', Validators.required],
+      upBSMId: ['', Validators.required],
+      updateDate: ['', Validators.required],
+      updatedBy: ['', Validators.required],
+      userName: ['', Validators.required],
+      userRefId: ['', Validators.required],
+      villRefId: ['', Validators.required],
+      villagePanchayatId: ['', Validators.required],
+      
+});
+
   }
 
 }
