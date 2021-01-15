746bc5137feb808d539c320e46413b3868be9a86
Neosoft <vaishali.patil2725@gmail.com>
2021-01-15 14:42:56 +0530

    code for interceptor, base url for api

---
+++ src/app/_services/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import {environment}  from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  _url: string;
+  constructor(private http: HttpClient) { 
+    this._url = environment.baseUrl;
+  }
+
+  userLogin(data){
+    alert()
+    return  this.http.post<any>(this._url+'signin', data).pipe(map((res:any)=>{
+      return res;
+    }));
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'https://52.151.57.108:8082/',
 };
 
 /*
