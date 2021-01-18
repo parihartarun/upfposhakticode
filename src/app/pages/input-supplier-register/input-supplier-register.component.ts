88c1b7882af19318d533a91ca84672e7674ec42f
vishal patil <vishalp755@gmail.com>
2021-01-18 14:31:46 +0530

    chnage  for districtRefId id

---
+++ src/app/pages/input-supplier-register/input-supplier-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-supplier-register',
+  templateUrl: './input-supplier-register.component.html',
+  styleUrls: ['./input-supplier-register.component.css']
+})
+export class InputSupplierRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
