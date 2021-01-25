c44795a7cd5ea05f2591e18b9162b7a8c9577c72
rakhee123 <rakhee.jain@neosoftmail.com>
2021-01-25 18:34:31 +0530

    done

--- src/app/pages/registers/farmer-register/farmer-register.component.ts
+++ src/app/pages/registers/farmer-register/farmer-register.component.ts
@@ -4,7 +4,6 @@ import { MustMatch } from '../../../_helpers/constomMatchValidor';
 import { AuthService } from '../../../_services/auth/auth.service';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/internal/Observable';
-import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -24,8 +23,7 @@ export class FarmerRegisterComponent implements OnInit {
   panchayts = [];
   villages = [];
   banks = [];
-
-  constructor(private fb: FormBuilder, private api: AuthService, private _router: Router, private toastr: ToastrService) {
+  constructor(private fb: FormBuilder, private api: AuthService, private _router: Router) {
   }
 
   ngOnInit() {
@@ -73,15 +71,14 @@ export class FarmerRegisterComponent implements OnInit {
       distRefId: ['', Validators.required],
       gender: ['', Validators.required],
       createdBy:'ROLE_FARMER' ,
-      roleRefId: 6,
       deleted: [true],
       enabled: [true],
-      farmerMob: ['', [Validators.required, Validators.pattern("[0-9 ]{10}")]],
+      farmerMob: 0,
       farmerName: ['', Validators.required],       
       ifscCode: ['', Validators.required],
       parantsName: ['', Validators.required],
-      pincode: ['', [Validators.required, Validators.pattern("[0-9 ]{6}")]],
-      userName: ['', [Validators.required, Validators.pattern("[0-9a-zA-Z]{6,20}")]],
+      pincode: ['', Validators.required],
+      userName: ['', Validators.required],
       userRefId: [''],
       villRefId: ['', Validators.required],
       villagePanchayatId: ['', Validators.required],
@@ -94,6 +91,8 @@ export class FarmerRegisterComponent implements OnInit {
        
     })
 
+
+
   }
   get formControls() {
     return this.registerForm.controls;
@@ -102,6 +101,8 @@ export class FarmerRegisterComponent implements OnInit {
     return this.registerForm.get('password');
   }  
   register(): Observable<any>  {
+    alert("ddfgdfgdh")
+    console.log(JSON.stringify(this.registerForm.value));
     this.submitted = true;
     // stop here if form is invalid
     if (this.registerForm.invalid) {
@@ -111,26 +112,44 @@ export class FarmerRegisterComponent implements OnInit {
    
     let user = {
       userName: this.registerForm.value.userName,
-      password: this.registerForm.value.password,
-      roleRefId: 6,
+      password: this.registerForm.value.password
     }
     delete this.registerForm.value.password;
     delete this.registerForm.value.userName;
-    delete this.registerForm.value.confirmPassword;  
-
+    delete this.registerForm.value.confirmPassword;
+    this.registerForm.value.userFar = user;
+    //let famerRegister = {
+    //  accountNo: '',
+    //  bankRefId: '',
+    //  blockRef: '',
+    //  category: '',
+    //  confirmPassword: '',
+    //  deleted: true,
+    //  distRefId: '',
+    //  enabled: true,
+    //  farmerMob:'',
+    //  farmerName: '',
+    //  gender:'',
+    //  ifscCode: '',
+    //  parantsName: '',
+    //   password: '',
+    //  pincode: '',
+    //  recaptcha:'',
+    //  user: user,
+    //  userRefId: "",
+    //  villRefId: "",
+    //  villagePanchayatId: ""
+    //}
+    //famerRegister.accountNo=
+    
+    console.log(JSON.stringify(this.registerForm.value));
     this.api.registerUser(this.registerForm.value).subscribe(response => {
-      if (response.message == "SuccessFully Saved!") {
-        this.toastr.success(response.message);
-        this.registerForm.reset();
-        this._router.navigate(['/login'])
-      }
-      else {
-        this.toastr.error(response.message);
-      }
+      alert(response.message);
+     
    },
       err => {
         console.log(err);
-       
+        alert(err);
       })
      
   }
