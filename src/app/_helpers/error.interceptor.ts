383ef0fda10edda95fb63fd4a8515d3d8d7f3fb3
Neosoft <vaishali.patil2725@gmail.com>
2021-01-26 10:55:58 +0530

    catch exception in interceptor

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -3,10 +3,11 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Router } from "@angular/router";
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private router: Router) {}
+    constructor(private router: Router, private toastr:ToastrService) {}
 
     intercept(
       request: HttpRequest<any>,
@@ -15,19 +16,19 @@ export class ErrorInterceptor implements HttpInterceptor {
       return next.handle(request).pipe(
         catchError(err => {
           console.log(err);
-          if (err.status === 401) {
-            // auto logout if 401 response returned from api
-            localStorage.removeItem("accessToken");
-            this.router.navigate(["login"]);
-          } else if (err.status == 500) {
-            this.router.navigate(["error", 500]);
-          } else if (err.status == 400) {
-            this.router.navigate(["error", 400]);
-          } else if (err.status == 400) {
-            this.router.navigate(["error", 400]);
-          }
-          const error = err.statusText;
-          return throwError(error);
+          this.toastr.error(err.error.message);
+          // if (err.status === 401) {
+          //   localStorage.removeItem("accessToken");
+          //   this.router.navigate(["login"]);
+          // } else if (err.status == 500) {
+          //   this.router.navigate(["error", 500]);
+          // } else if (err.status == 400) {
+          //   this.router.navigate(["error", 400]);
+          // } else if (err.status == 400) {
+          //   this.router.navigate(["error", 400]);
+          // }
+           const error = err.statusText;
+           return throwError(error);
         })
       );
     }
