bf43617b99ecfd94fe7e04d32d4fd0270074fb32
vishal patil <vishalp755@gmail.com>
2021-02-02 14:52:02 +0530

    change for home page

--- src/app/layouts/auth-layout/auth-layout.module.ts
+++ src/app/layouts/auth-layout/auth-layout.module.ts
@@ -5,17 +5,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthLayoutRoutes } from './auth-layout.routing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { RegisterComponent } from '../../pages/registers/register.component';
+
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { BsDatepickerConfig, BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { NgxCaptchaModule } from 'ngx-captcha';
-import { FpoRegisterComponent } from '../../pages/registers/fpo-register/fpo-register.component';
-import { FarmerRegisterComponent } from '../../pages/registers/farmer-register/farmer-register.component';
-import { ByerSellRegisterComponent } from '../../pages/registers/byer-sell-register/byer-sell-register.component';
-import { InputSupplierRegisterComponent } from '../../pages/registers/input-supplier-register/input-supplier-register.component';
-import { EquipmentCentreRegisterComponent } from '../../pages/registers/equipment-centre-register/equipment-centre-register.component';
-import { FarmerChcRegisterComponent } from '../../pages/registers/farmer-chc-register/farmer-chc-register.component';
 
 
 @NgModule({
@@ -30,14 +24,8 @@ import { FarmerChcRegisterComponent } from '../../pages/registers/farmer-chc-reg
     NgbModule
   ],
   declarations: [
-    RegisterComponent,
-    FarmerRegisterComponent,    
-   
-    FpoRegisterComponent,
-    ByerSellRegisterComponent,
-    InputSupplierRegisterComponent,
-    EquipmentCentreRegisterComponent,
-    FarmerChcRegisterComponent
+    
+    
   ],
   providers: [
     { provide: BsDatepickerConfig },
