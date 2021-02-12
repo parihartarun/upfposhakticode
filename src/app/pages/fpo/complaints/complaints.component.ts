97d8ce2736c95ee6da802108d45d7520a8ae52d1
vishal patil <vishalp755@gmail.com>
2021-02-12 13:35:44 +0530

    chnage for famer compaint

--- src/app/pages/fpo/complaints/complaints.component.ts
+++ src/app/pages/fpo/complaints/complaints.component.ts
@@ -187,12 +187,12 @@ export class ComplaintsComponent implements OnInit {
   viewComplaint(complaint) {
     this.isViewComplaint = true;
     this.viewComp.farmerId = complaint.farmerId
-    this.viewComp.assignedTo = complaint.assignTo;
+    this.viewComp.assignedTo = complaint.assignBy;
     this.viewComp.assigned_date = complaint.assigned_date;
     this.viewComp.currentStatus = complaint.status;
     this.viewComp.description = complaint.description;
     this.viewComp.compalintDate = complaint.uploadDate;
-    this.viewComp.remarks = complaint.remarks;
+    this.viewComp.remarks = complaint.fpoComment;
     this.viewComp.title = complaint.title;
     window.scroll(0, 0);
     let myDate = new Date();
