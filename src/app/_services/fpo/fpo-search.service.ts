2dfe66a4388a5ca82f53d5e3497c7fd90a8b2407
vaishali2725 <62466498+vaishali2725@users.noreply.github.com>
2021-02-24 13:35:19 +0530

    update

--- src/app/_services/fpo/fpo-search.service.ts
+++ src/app/_services/fpo/fpo-search.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -8,14 +9,27 @@ import { environment } from 'src/environments/environment';
 export class FpoSearchService {
 
   _url: string;
+  districtObserver: BehaviorSubject<any> = new BehaviorSubject([]);
+  fpoObserver: BehaviorSubject<any> = new BehaviorSubject([]);
 
   constructor(private http: HttpClient) {
     this._url = environment.baseUrl;
   }
 
-  getDistrictBystateId(keyword, type) {
+  getDistrict(keyword, type) {
     this.http.get<any>(this._url + `api/search/filters/districts?in=${type}&val=${keyword}`).subscribe((res: any) => {
-      console.log('district list', res);
+      this.districtObserver.next(res);
+    })
+  }
+  getFpo(keyword, type) {
+    this.http.get<any>(this._url + `api/search/filters/fpos?in=${type}&val=${keyword}`).subscribe((res: any) => {
+      this.fpoObserver.next(res);
+    })
+  }
+  getCrops(keyword, type) {
+    this.http.get<any>(this._url + `api/search/filters/crops?in=${type}&val=${keyword}`).subscribe((res: any) => {
+      // this.fpoObserver.next(res);
+      console.log('all crops', res);
 
     })
   }
