1fe64db553dbccfcfaff2294a2c8b4a78ad2b9ca
Vidhi Shrivastava <vidhi.shrivastava@neosoftmail.com>
2021-03-26 20:46:14 +0530

    added contact-person to CHCFmb profile

--- src/app/pages/CHC/chcfmb-profile/chcfmb-profile.component.ts
+++ src/app/pages/CHC/chcfmb-profile/chcfmb-profile.component.ts
@@ -58,8 +58,10 @@ export class ChcfmbProfileComponent implements OnInit {
   getUserData(){
     this.chcService.getUserDetails(this.userId).subscribe(data=>{ 
       this.profileData = data.chcFmb;
+      
      this.api.getBlock(parseInt(this.profileData.district_id)).subscribe(block => {
         this.blocks = block;
+        
       })
       this.api.getVillageByBlock(parseInt(this.profileData.block_id)).subscribe(v => {
         this.villages = v;
@@ -70,7 +72,7 @@ export class ChcfmbProfileComponent implements OnInit {
         distRefId: this.profileData.district_id,
         blockRefId: this.profileData.block_id,
         villageRefId:this.profileData.village_id,
-        contactPerson: this.profileData.user_name, //need to change for contact person
+        contactPerson: this.profileData.contactPerson, 
         mobileNumber: this.profileData.mobile_number,
         pincode: this.profileData.pincode,
         firmRegistraionNumber: this.profileData.firm_registraion_number ,
@@ -110,7 +112,7 @@ export class ChcfmbProfileComponent implements OnInit {
     }
     let famerCHCFmb = this.chcprofileForm.value;
     this.chcService.updateChcProfile(famerCHCFmb).subscribe(response => {
-      if(response.chcFmbId != ''){
+      if(response.chcFmbId != ''){        
         this.submitted = false;
         this.toastr.success('Profile Updated successfully.');
       }else{
